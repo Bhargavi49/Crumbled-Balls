aef6f1a5d6ef00d57f53d787e64c31221516b1a7
Bhargavi49 <75205619+Bhargavi49@users.noreply.github.com>
2020-12-15 16:18:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, gameState,engine, world,dustbin,paper;
+function setup() {
+  createCanvas(800, 400);
+  rectMode(CENTER);
+
+  gameState = "start";
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+  
+  
+    dustbin.display();
+    paper.display();
+
+  Matter.Body.applyForce(paper.body, paper.body.position,)
+     
+  
+  }
